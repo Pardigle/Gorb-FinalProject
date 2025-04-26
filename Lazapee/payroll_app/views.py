b0969615c122b32806bf635298a8a066b556b4b5
pardige <140949466+Pardigle@users.noreply.github.com>
2025-04-26 15:27:51 +0800

    Set-up HTML files.

--- Lazapee/payroll_app/views.py
+++ Lazapee/payroll_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from models import Employee, Payslip
 
 # Create your views here.
+def employees_page(request):
+    employees = Employee.objects.all()
+    return render(request, 'payroll_app/employees_page.html', {'employees':employees})
+
