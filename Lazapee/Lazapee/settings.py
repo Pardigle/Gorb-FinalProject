b31e9115a446d4eb0882b16af30f3bc139cc78aa
pardige <140949466+Pardigle@users.noreply.github.com>
2025-05-11 20:57:26 +0800

    Minor edits

--- Lazapee/Lazapee/settings.py
+++ Lazapee/Lazapee/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'payroll_app'
+    'django.contrib.humanize',
+    'payroll_app',
 ]
 
 MIDDLEWARE = [
